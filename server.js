333dee3ce6202483269d275a7807ff8b357e9ed6
MAZAH11 <160371264+MAZAH11@users.noreply.github.com>
2024-07-16 18:27:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //});
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'Q8.html'));
 });
 
 app.post('/', (req, res) => {
